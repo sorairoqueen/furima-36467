0259823d5ce720be30f16189d9d960d8ba6a16c6
sorairoqueen <48656738+sorairoqueen@users.noreply.github.com>
2021-11-13 17:38:15 +0900

    itemモデルのバリデーション実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,18 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true }
+
+  validates :item_name, :content, presence: true
+
+  validates :item_category_id, :item_status_id, :fee_resp_id, :prefecture_id, :delivery_term_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  belongs_to :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :item_category
   belongs_to :item_status
   belongs_to :fee_resp
+  belongs_to :prefecture
   belongs_to :delivery_term
 
 end
