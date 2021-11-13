a9ea8b232cf8b551a9501e2db1793753021ad72d
sorairoqueen <48656738+sorairoqueen@users.noreply.github.com>
2021-11-13 13:03:20 +0900

    itemsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_24_075719) do
+ActiveRecord::Schema.define(version: 2021_11_13_035308) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "item_name"
+    t.integer "price"
+    t.integer "item_category_id"
+    t.integer "item_status_id"
+    t.integer "fee_resp_id"
+    t.integer "prefecture_id"
+    t.integer "delivery_term_id"
+    t.string "content"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
