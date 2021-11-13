214de4e62ffee9e44e9486c9ee387fba09bb9b33
sorairoqueen <48656738+sorairoqueen@users.noreply.github.com>
2021-11-13 16:28:42 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_status
+  belongs_to :fee_resp
+  belongs_to :delivery_term
+
 end
