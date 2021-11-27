7c7a067d40eaea62b11bf0368a778f3bd1a20976
sorairoqueen <48656738+sorairoqueen@users.noreply.github.com>
2021-11-27 13:43:31 +0900

    エラーハンドリング実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,11 +30,10 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
       redirect_to root_path
     else
-      @item = Item.find(params[:id])
       render :edit
     end
   end
