5264a6de7a2dfbfa59db74e5e9190ef50a06fe61
sorairoqueen <48656738+sorairoqueen@users.noreply.github.com>
2021-12-19 22:04:27 +0900

    フォーム情報の保存(3)

--- app/models/order_delivery_address.rb
+++ app/models/order_delivery_address.rb
@@ -1,6 +1,6 @@
 class OrderDeliveryAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :order_id, :delivery_postalcode, :prefecture_id, :delivery_city, :delivery_block, :delivery_building, :orderer_phone_num
+  attr_accessor :order_id, :user_id, :item_id, :delivery_postalcode, :prefecture_id, :delivery_city, :delivery_block, :delivery_building, :orderer_phone_num
 
   with_options presence: true do
     validates :delivery_postalcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
@@ -8,13 +8,13 @@ class OrderDeliveryAddress
               numericality: { other_than: 1, message: "can't be blank" }
     validates :delivery_city, :delivery_block
     validates :orderer_phone_num, format: { with: /[0-9]{10,11}/ }
-    validates :user_id, :item_id, :order_id
+    validates :user_id, :item_id
   end
   def save
-    order = Order.save(
+    order = Order.create(
       user_id: user_id, item_id: item_id
     )
-    Delivery_address.create(
+    DeliveryAddress.create(
       order_id: order.id, delivery_postalcode: delivery_postalcode, prefecture_id: prefecture_id, delivery_city: delivery_city, delivery_block: delivery_block, delivery_building: delivery_building, orderer_phone_num: orderer_phone_num
     )
   end
