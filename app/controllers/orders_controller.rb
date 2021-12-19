0c4ba6b0a33b93d97c22cfc395d601df638cbcef
sorairoqueen <48656738+sorairoqueen@users.noreply.github.com>
2021-12-19 16:32:10 +0900

    フォーム情報の保存(1)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,22 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
-    @OrderDeliveryaddress = OrderDeliveryaddress.new()
+    @OrderDeliveryAddress = OrderDeliveryAddress.new()
   end
   def create
+    @OrderDeliveryAddress = OrderDeliveryAddress.new(order_save)
+    if @OrderDeliveryAddress.valid?
+      @OrderDeliveryAddress.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+
+  def order_save
+    params.require(:order_delivery_address).permit(:delivery_postalcode, :prefecture_id, :delivery_city, :delivery_block, :delivery_building, :orderer_phone_num).merge(user_id: current_user.id)
   end
 end
--- app/models/order_delivery_address.rb
+++ app/models/order_delivery_address.rb
@@ -1,6 +1,6 @@
-class OrderDeliveryaddress
+class OrderDeliveryAddress
   include ActiveModel::Model
-  attr_accessor :user, :item, :order, :delivery_postalcode, :prefecture_id, :delivery_city, :delivery_block, :delivery_building, :orderer_phone_num
+  attr_accessor :user_id, :item_id, :order_id, :delivery_postalcode, :prefecture_id, :delivery_city, :delivery_block, :delivery_building, :orderer_phone_num
 
   with_options presence: true do
     validates :delivery_postalcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
@@ -11,8 +11,11 @@ class OrderDeliveryaddress
     validates :user_id, :item_id, :order_id
   end
   def save
-    Deliveryaddress.create(
-      delivery_postalcode: delivery_postalcode, prefecture_id: prefecture_id, delivery_city: delivery_city, delivery_block: delivery_block, delivery_building: delivery_building, orderer_phone_num: orderer_phone_num
+    order = Order.save(
+      user_id: user_id, item_id: item_id
+    )
+    Delivery_address.create(
+      order_id: order.id, delivery_postalcode: delivery_postalcode, prefecture_id: prefecture_id, delivery_city: delivery_city, delivery_block: delivery_block, delivery_building: delivery_building, orderer_phone_num: orderer_phone_num
     )
   end
 end
